e83d785b8b9b72d36daa6dc558f0d6d38313cc76
n1kolayvas1lev <ckb01@ya.ru>
2021-12-23 21:35:53 +0300

    23.12.2021 - Добавлена аннотация типов; - Добавлена документация к функциям; - Заменены циклы на List comp.

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import json
 from conf import model as MODEL
 
 
-def book_gen(pk=1):
+def book_gen(pk: int = 1) -> dict:
+    """Генератор словаря-описания и счётчика.
+    Возвращает dict.
+    """
     while True:
         yield {
             "model": MODEL,
@@ -23,49 +26,69 @@ def book_gen(pk=1):
         pk += 1
 
 
-def title():
+def title() -> str:
+    """Получение названия книги из файла.
+    Название избавляется от непечатаемых символов.
+    Возвращает str.
+    """
     with open('books.txt', 'r', encoding='utf-8',) as books:
         l_books = books.readlines()
         l_books = [l_b.rstrip() for l_b in l_books]
         return random.choice(l_books)
 
 
-def year():
+def year() -> int:
+    """Генератор года выпуска книги. Возвращает int."""
     return random.randint(900, 2021)
 
 
-def pages():
+def pages() -> int:
+    """Генератор количества страниц. Возвращает int."""
     return random.randint(1, 999999)
 
 
-def isbn():
+def isbn() -> Faker:
+    """Генератор ISBN. Возвращает объект Faker."""
     fake = Faker()
     return fake.isbn10()
 
 
-def rating():
+def rating() -> int:
+    """Генератор рейтинга книги. Возвращает int."""
     return random.randint(0, 5)
 
 
-def price():
-
+def price() -> float:
+    """Генератор ценника. Возвращает float."""
     return random.uniform(1, 999999)
 
 
-def author():
+def author() -> list:
+    """Генератор списка авторов:
+    i -- случайное значение от 1 до 3, задаёт количество случайных имён;
+    fake.name() -- случайное имя
+    authors(i, fake name) -> list, список авторов
+    """
     i = random.randint(1, 3)
-    authors = []
     fake = Faker()
-    for _ in range(i):
-        authors.append(fake.name())
+    authors = [fake.name() for _ in range(i)]
+
+    # for _ in range(i):
+    #     authors.append(fake.name())
     return authors
 
 
-def main(output_file: str):
+def main(output_file: str) -> None:
+    """Создание файла списка словарей:
+    cnt -- количество книг запрашивается у пользователя;
+    book_generator -- итератор возвращающий словарь с содержимым и счётчик;
+    list_books -- список книг, записываемый в файл
+    """
+    cnt = int(input('Сколько нужно книг? '))
     book_generator = book_gen()
-    list_books = []
-    for _ in range(100):
-        list_books.append(next(book_generator))
+    list_books = [next(book_generator) for _ in range(cnt)]
+    # for _ in range(100):
+    #     list_books.append(next(book_generator))
     with open(out_file, 'w') as outp:
         json.dump(list_books, outp, indent=4, ensure_ascii=False)
 #    return list_books
